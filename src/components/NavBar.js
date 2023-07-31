40747def215f073b8fe3acf9fce72145869c3f36
DieDab070580 <diedab@gmail.com>
2023-07-30 23:00:20 -0300

    primera parte del NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+function NavBar () {
+  return (
+    <>
+    <div className="container">
+      <nav className="nav">
+        <div className="nav_brand">
+          <a className="nav_link" href="#">MiMarca</a>
+        </div>
+        <ul className="nav_list">
+          <li>
+            <a className="nav_link" href="#">Quienes somos</a>
+          </li>
+          <li>
+            <a className="nav_link" href="#">Carrito</a>
+          </li>
+          <li>
+            <a className="nav_link" href="#">Carrito</a>
+          </li>
+        </ul>  
+      </nav>
+    </div>
+    </>
+  );
+};
+
+export default NavBar;
